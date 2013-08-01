5b2695064f85cb447cf3de959fedce85f8e5b295
Paul Rohorzka <paul.rohorzka@softconcept.at>
2013-08-01 04:51:24 +0200

    Extracted class EnumeratorExtensions

---
+++ WikiTools/EnumeratorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WikiTools
+{
+    public static class EnumeratorExtensions
+    {
+        public static IEnumerable<TTarget> TakeWhileOfType<T, TTarget>(this IEnumerator<T> enumer)
+            where TTarget : T
+        {
+            while (enumer.MoveNext() && enumer.Current is TTarget)
+            {
+                yield return (TTarget)enumer.Current;
+            }
+        }
+    }
+}
+
--- WikiTools/Grammar/Line.cs
+++ WikiTools/Grammar/Line.cs
@@ -21,16 +21,4 @@ namespace WikiTools.Grammar
             };
         }
     }
-
-    public static class EnumeratorExtensions
-    {
-        public static IEnumerable<TTarget> TakeWhileOfType<T, TTarget>(this IEnumerator<T> enumer)
-            where TTarget : T
-        {
-            while (enumer.MoveNext() && enumer.Current is TTarget)
-            {
-                yield return (TTarget)enumer.Current;
-            }
-        }
-    }
 }
