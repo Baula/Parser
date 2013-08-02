cef2dbd055df86123d400927af57634156ea37b7
Paul Rohorzka <paul.rohorzka@softconcept.at>
2013-08-03 00:45:56 +0200

    Streamlined Identifier

--- ParserTechPlayground/Identifier.cs
+++ ParserTechPlayground/Identifier.cs
@@ -5,35 +5,32 @@ namespace ParserTechPlayground
 {
     public class Identifier
     {
-        private List<Character> _characters;
         private string _name;
 
         public Identifier(List<Character> characters)
         {
-            _characters = characters;
             _name = characters.Aggregate("", (s, c) => s += c.Value);
         }
 
-        public string Name
-        {
-            get { return _name; }
-        }
+        public string Name { get { return _name; } }
 
         // Identifier   : Character+
         internal static Identifier Produce(TokenBuffer tokens)
         {
             var characters = new List<Character>();
             var character = tokens.GetTerminal<Character>();
-            if (character == null)
-                return null;
-
-            while (character != null)
+            if (character != null)
             {
-                characters.Add(character);
-                character = tokens.GetTerminal<Character>();
+                while (character != null)
+                {
+                    characters.Add(character);
+                    character = tokens.GetTerminal<Character>();
+                }
+
+                return new Identifier(characters);
             }
 
-            return new Identifier(characters);
+            return null;
         }
     }
 }
