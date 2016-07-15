cf81fa18997fcd777874a8f4a3d6e393d83cd554
Paul Rohorzka <paul@paulroho.com>
2016-07-15 15:53:38 +0200

    Added sample production and a test projects for dotnet core

---
+++ test/CoreLib.Tests/CoreTestClass.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreLib.Tests
+{
+    [TestClass]
+    public class CoreTestClass
+    {
+        [TestMethod]
+        public void CoreTestMethod()
+        {
+            var coreClass = new CoreLibClass();
+
+            coreClass.Name = "Hallo";
+
+            Assert.AreEqual("Hallo", coreClass.Name);
+        }
+    }
+}
